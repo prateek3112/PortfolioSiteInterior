3e5a2c44e487e212a1687af73ff4458b1b5b793f
prateek3112 <prateekguglani1998@gmail.com>
2024-03-16 22:32:25 +0530

    fix

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,7 @@ import { TbArrowUpRight } from "react-icons/tb";
 export default function About() {
     return (
         <div>
-            <div className="bg-[url('/image/about.jpg')] bg-center bg-cover ">
+            <div className="bg-[url(&apos;/image/about.jpg&apos;)] bg-center bg-cover ">
                 <h1 className="container py-32 text-6xl font-semibold tracking-widest text-center text-white lg:py-64 lg:text-left ">WHO ARE WE?</h1>
             </div>
             <div className="container">
@@ -20,15 +20,15 @@ export default function About() {
                     </div>
                     <div className="">
                         <p className="pb-8 tracking-wide">
-                        At Saumyaksh, we're visionary interior designers committed to revolutionizing architectural education. We believe in the imperative to reimagine conventional approaches, infusing them with contemporary methodologies and cutting-edge tools. Our ethos is rooted in the seamless integration of diverse societal perspectives, alongside innovative techniques in creativity, production, and communication.
+                            At Saumyaksh, we&apos;re visionary interior designers committed to revolutionizing architectural education. We believe in the imperative to reimagine conventional approaches, infusing them with contemporary methodologies and cutting-edge tools. Our ethos is rooted in the seamless integration of diverse societal perspectives, alongside innovative techniques in creativity, production, and communication.
 
-Crafted with precision and passion, our Unfolding Box stands as a testament to our dedication. Designed to house and exhibit the exquisite portfolio of our second-year architecture endeavors, it symbolizes our unwavering commitment to excellence and creativity.                            <br />
+                            Crafted with precision and passion, our Unfolding Box stands as a testament to our dedication. Designed to house and exhibit the exquisite portfolio of our second-year architecture endeavors, it symbolizes our unwavering commitment to excellence and creativity.
+                            <br />
                             <br />
                         </p>
                         <a className="inline-flex items-center gap-1 px-6 py-3 text-sm text-white rounded-full shadow-lg bg-gray-950 hover:bg-gray-800 hover:ring-2 hover:ring-gray-950 ring-offset-2" href="">Read More <TbArrowUpRight className="text-xl" /> </a>
                     </div>
                 </div>
- 
             </div>
         </div>
     );
